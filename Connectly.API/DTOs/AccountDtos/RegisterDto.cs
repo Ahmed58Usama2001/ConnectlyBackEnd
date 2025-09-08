6e37e8e23c9ef50891fe609dd99c78f908a15673
Ahmed Usama <ausama@innotech.com.eg>
2025-09-08 20:05:06 +0300

    adding confirm password to register dto

--- Connectly.API/DTOs/AccountDtos/RegisterDto.cs
+++ Connectly.API/DTOs/AccountDtos/RegisterDto.cs
@@ -7,10 +7,14 @@ public class RegisterDto
     public string Email { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "User Name is required.")]
-    [MinLength(3)]
-    [MaxLength(20)]
+    [MinLength(3, ErrorMessage = "Username must be at least 3 characters long.")]
+    [MaxLength(20, ErrorMessage = "Username cannot exceed 20 characters.")]
     public string UserName { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Password is required.")]
     public string Password { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Confirm Password is required.")]
+    [Compare("Password", ErrorMessage = "Password and Confirm Password do not match.")]
+    public string ConfirmPassword { get; set; } = string.Empty;
 }

97bf35b596c197f1c7b49bbd3c52f3159580632d
Ahmed Usama <ausama@innotech.com.eg>
2025-09-23 13:10:19 +0300

    making the messages contoller with adding messae endpoint

---
+++ Connectly.API/Controllers/MessagesController.cs
@@ -0,0 +1,44 @@
+﻿using Connectly.API.DTOs.MessagesDtos;
+
+namespace Connectly.API.Controllers;
+
+[Authorize]
+public class MessagesController(IMessageRepository _messageRepository,
+    UserManager<AppUser> _userManager,
+    IMapper _mapper) : BaseApiController
+{
+
+    [HttpPost]
+    public async Task<ActionResult<MessageDto>> CreateMessage(CreateMessageDto createMessageDto)
+    {
+        var publicIdString = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var sender = await GetUserByPublicId(publicIdString!);
+
+        var recipient = await GetUserByPublicId(createMessageDto.RecipientId.ToString());
+
+        if (sender == null || recipient == null || sender.PublicId == createMessageDto.RecipientId)
+            return NotFound("Can not send this message");
+
+        var message = new Message
+        {
+            SenderId = sender.Id,
+            RecipientId = recipient.Id,
+            Content = createMessageDto.Content
+        };
+
+        _messageRepository.Add(message);
+
+        if (await _messageRepository.SaveAllAsync())
+            return _mapper.Map<MessageDto>(message);
+
+        return BadRequest("Failed to send message");
+    }
+
+    private async Task<AppUser?> GetUserByPublicId(string publicIdString)
+    {
+        if (!Guid.TryParse(publicIdString, out var publicId))
+            return null;
+
+        return await _userManager.Users.SingleOrDefaultAsync(u => u.PublicId == publicId);
+    }
+}
--- Connectly.API/Helpers/MappingProfiles.cs
+++ Connectly.API/Helpers/MappingProfiles.cs
@@ -1,4 +1,6 @@
-﻿namespace Connectly.API.Helpers;
+﻿using Connectly.API.DTOs.MessagesDtos;
+
+namespace Connectly.API.Helpers;
 
 public class MappingProfiles : Profile
 {
@@ -6,5 +8,14 @@ public class MappingProfiles : Profile
     {
         CreateMap<AppUser, MemberDto>()
             .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.PublicId.ToString()));
+        CreateMap<Message, MessageDto>()
+            .ForMember(dest => dest.SenderId, opt => opt.MapFrom(src => src.Sender.PublicId.ToString()))
+            .ForMember(dest => dest.SenderUserName, opt => opt.MapFrom(src => src.Sender.UserName))
+            .ForMember(dest => dest.SenderImageUrl, opt => opt.MapFrom(src => src.Sender.ImageUrl))
+
+            .ForMember(dest => dest.RecipientId, opt => opt.MapFrom(src => src.Recipient.PublicId.ToString()))
+            .ForMember(dest => dest.RecipientUserName, opt => opt.MapFrom(src => src.Recipient.UserName))
+            .ForMember(dest => dest.RecipientImageUrl, opt => opt.MapFrom(src => src.Recipient.ImageUrl));
+
     }
 }

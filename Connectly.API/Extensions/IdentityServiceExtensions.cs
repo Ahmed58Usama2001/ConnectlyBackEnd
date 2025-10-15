10d75f530ec3c3857f1d0f3f7f9718d9cb2e1562
Ahmed Usama <ausama@innotech.com.eg>
2025-10-15 20:25:42 +0300

    configuring auth to signalR

--- Connectly.API/Extensions/IdentityServiceExtensions.cs
+++ Connectly.API/Extensions/IdentityServiceExtensions.cs
@@ -40,7 +40,20 @@ public static class IdentityServiceExtensions
                 ClockSkew = TimeSpan.FromDays(double.Parse(configuration["JWT:AccessTokenDurationInMinutes"] ?? string.Empty))
             };
 
-            
+            options.Events = new JwtBearerEvents
+            {
+                OnMessageReceived = context =>
+                {
+                    var accessToken = context.Request.Query["access_token"];
+
+                    var path = context.HttpContext.Request.Path;
+                    if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+                    {
+                        context.Token = accessToken;
+                    }
+                    return Task.CompletedTask;
+                }
+            };
         });
 
         services.AddAuthorizationBuilder()
